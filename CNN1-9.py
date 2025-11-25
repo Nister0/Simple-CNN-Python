60ecc1d79edf8b6671b4f48fcb31f217c0340ff0
Nister0 <benjamin-simon@t-online.de>
2025-11-25 14:30:21 +0100

    fixed padding function -still giving Gobject errors but works fine.

--- CNN1-9.py
+++ CNN1-9.py
@@ -21,9 +21,10 @@ class Greyscaler:
         pass
 
     def average(self, image):
-        ''' the average function converts RGB Images to Greyscale
-            Based on the Average of each color.
-            Easy to implement but not optimal
+        ''' 
+        the average function converts RGB Images to Greyscale
+        Based on the Average of each color.
+        Easy to implement but not optimal
         '''
         # ensure image is RGB so getpixel returns an (r,g,b) tuple
         image = image.convert("RGB")
@@ -53,24 +54,25 @@ class Conv:
 
         padding = self.padding
 
-        new_w, new_h = w+(padding*2), h+(padding*2)
+        new_w = w+(padding*2)
+        new_h = h+(padding*2)
 
         padded_image = np.zeros((new_h, new_w, 3), dtype =np.uint8)
 
         for i in range(new_h):
             for j in range(new_w):
                 if i < padding:
-                    padded_image[i,j] = (255,255,255)
-                elif i >= new_w-padding:
-                    padded_image[i,j] = (255,255,255)
+                    padded_image[i,j] = (255,110,255)
+                elif i >= new_h-padding:
+                    padded_image[i,j] = (255,110,255)
                 elif j < padding: 
-                    padded_image[i,j] = (255,255,255)
-                elif j >= new_h-padding:
-                    padded_image[i,j] = (255,255,255)
+                    padded_image[i,j] = (255,110,255)
+                elif j >= new_w-padding:
+                    padded_image[i,j] = (255,110,255)
                 else:
                     print((i,j))
-                    print((new_w, new_h, w, h, padding))
-                    r,g,b = image.getpixel((i-padding, j-padding))
+                    print((new_h, new_w, h, w, padding))
+                    r,g,b = image.getpixel((j-padding, i-padding))
                     padded_image[i,j] = (r,g,b)
 
         return padded_image
@@ -87,6 +89,7 @@ im = Image.open("Testimage.png")
 greyscale = Greyscaler()
 grey_output = greyscale.average(im)
 imgr = Image.fromarray(grey_output)
+imgr.show()
 
 arr = np.zeros((5,5,3), dtype = np.uint8)
 for i in range(5):
@@ -97,6 +100,7 @@ for i in range(5):
             arr[i,j] = (50, 100, 255)
 
 testimg = Image.fromarray(arr)
-conv = Conv(3, 1, 20)
+
+conv = Conv(3, 1, 1)
 impadded = Image.fromarray(conv.pad_image(imgr))
 impadded.show()
